b46e6cfc5ab5790d1b198b368f14a073b3a4d3da
paulom1982 <62001530+paulom1982@users.noreply.github.com>
2024-01-12 17:26:36 -0500

    v2

--- includes/dashboard.js
+++ includes/dashboard.js
@@ -4,57 +4,120 @@
 
 // GET TRANSACTIONS LIST
 function getTransactions() {
-    // Get transaction list and load to table; 
+    
+    var now = moment();
+    var current_year_month = now.format('YYYY-MM');
     
     // USING THE SHEETDB METHOD.
     //    Will likely uste the SheetDB method instead since it supports enough requests for personal use and is much easier to use.  
     //    Using the Google API method below just to test an option that doesn't depend on a 3rd party such as SheetDB.
     $.getJSON('https://sheetdb.io/api/v1/n09q5n4vvki5y', function(myData) {
-        // JSON result in `data` variable
-        console.log(myData)
+
+        // JSON result in 'data' variable
         myDataJson = JSON.stringify(myData);
+
+        console.log(myData)
         console.log(myDataJson);
 
-        var table = new Tabulator("#transaction_table", {
-            height:305, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
-            data:myDataJson, //assign data to table
-            //layout:"fitColumns", //fit columns to width of table (optional)
-            columns:[ //Define Table Columns
-            {title:"Transaction Date", field:"Transaction_Date", sorter:"date", hozAlign:"center"},
-            {title:"Payee", field:"Payee"},
-            {title:"Category", field:"Category_1"},
-            {title:"Transaction Total", field:"Transaction_Total"}
+        // DATATABLES version:
+        new DataTable('#transaction_table_dt', {
+            responsive: true,
+            scrollY: 300,
+            deferRender: true,
+            scroller: true,            
+            data: myData,
+            order: [[0, 'desc']], 
+            // -----------------------------------------
+            // Creates the search/filter panes
+            dom: 'PBfrtip',
+            buttons: [
+                'copy', 'csv', 'excel', 'pdf', 'print'
             ],
+            searchPanes: {
+                initCollapsed: true,
+                columns: [2,4],       // only show search boxes for these columns,
+                cascadePanes: true,   // allows filtering based on values selected in other filters
+                viewTotal: true,      // view subtotals alongside totals for each value when filtering
+                dtOpts: {             // allows for multi-select on mobile devices  
+                    select: {
+                        style: 'multi'
+                    },
+                    order: [[0, 'asc']]
+                },
+                panes: [
+                    {
+                        header: 'Timeframe',
+                        options: [
+                            {
+                                label: 'Current Year',
+                                value: function(myData, rowIdx) {
+                                    var trans_date = myData['Transaction_Date'];
+                                    var current_year = moment(trans_date).isSame(new Date(), 'year');
+                                    return current_year == true;
+                                }
+                                //, className: 'tokyo'
+                            }, 
+                            {
+                                label: 'Current Month',
+                                value: function(myData, rowIdx) {
+                                    var trans_date = myData['Transaction_Date'];
+                                    var current_month = moment(trans_date).isSame(new Date(), 'month');
+                                    return current_month == true;
+                                }
+                            },
+                            {
+                                label: 'Previous Year',
+                                value: function(myData, rowIdx) {
+                                    var trans_date = myData['Transaction_Date'];
+                                    var previous_year = moment(trans_date).isBefore(new Date(), 'year');
+                                    return previous_year == true;
+                                }
+                                //, className: 'tokyo'
+                            }, 
+                            {
+                                label: 'Previous Month',
+                                value: function(myData, rowIdx) {
+                                    var trans_date = myData['Transaction_Date'];
+                                    var previous_month_start = moment().subtract(1, 'months').startOf('month');
+                                    var previous_month = moment(trans_date).isAfter(previous_month_start);
+                                    var current_month = moment(trans_date).isSame(new Date(), 'month');
+                                    return previous_month == true && current_month == false;
+                                }
+                            }
+                        ]                        
+                    },
+                    {
+                        header: 'Custom',
+                        options: [
+                            {
+                                label: 'Expenses Only',
+                                value: function(myData, rowIdx) {
+                                    return myData['Transaction_Type'] === 'Expense';
+                                }
+                            }, 
+                            {
+                                label: 'Groceries this Month',
+                                value: function(myData, rowIdx) {
+                                    // return myData[0] === '2023-12-09' && myData[3] === 'Groceries';
+                                    var trans_date = myData['Transaction_Date'];
+                                    return moment(trans_date, 'YYYY-MM-DD').format('YYYY-MM')  === current_year_month && myData['Category_1'] === 'Groceries';
+                                }
+                            }
+                        ]                        
+                    }
+                ]
+            },
+            // -----------------------------------------
+            columns: [
+                {title: 'Transaction Date', data: 'Transaction_Date'},
+                {title: 'Type', data: 'Transaction_Type'},
+                {title: 'Payee', data: 'Payee'},
+                {title: 'Total Amount', data: 'Transaction_Total'},
+                {title: 'Category', data: 'Category_1'},
+            ]
         });
-        
+
     });
-    
-    //Tabulator
-    // var table = new Tabulator("#transaction_table", {
-    //     ajaxURL:"https://sheetdb.io/api/v1/n09q5n4vvki5y", //ajax URL
-    // });
-
-
-    // TABULATOR SAMPLE:
-    // var tabledata = [
-    //   {id:1, name:"Oli Bob", age:"12", col:"red", dob:""},
-    //   {id:2, name:"Mary May", age:"1", col:"blue", dob:"14/05/1982"},
-    //   {id:3, name:"Christine Lobowski", age:"42", col:"green", dob:"22/05/1982"},
-    //   {id:4, name:"Brendon Philips", age:"125", col:"orange", dob:"01/08/1980"},
-    //   {id:5, name:"Margret Marmajuke", age:"16", col:"yellow", dob:"31/01/1999"},
-    // ];
-    // //create Tabulator on DOM element with id "example-table"
-    // var table = new Tabulator("#transaction_table", {
-    //   height:205, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
-    //   data:tabledata, //assign data to table
-    //   layout:"fitColumns", //fit columns to width of table (optional)
-    //   columns:[ //Define Table Columns
-    //     {title:"Name", field:"name", width:150},
-    //     {title:"Age", field:"age", hozAlign:"left", formatter:"progress"},
-    //     {title:"Favourite Color", field:"col"},
-    //     {title:"Date Of Birth", field:"dob", sorter:"date", hozAlign:"center"},
-    //   ],
-    // });
 
     // USING THE GOOGLE API METHOD.
     //    source: https://www.sitepoint.com/google-sheets-api-v4-integration-guide/
